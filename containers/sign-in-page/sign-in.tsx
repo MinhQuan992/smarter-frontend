52445a0145b5d77a4cff16ab74fbe2df84333a72
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2022-06-26 16:16:27 +0700

    feat: install Tailwind CSS, create a sign-in page and update README

--- containers/sign-in-page/sign-in.tsx
+++ containers/sign-in-page/sign-in.tsx
@@ -1,5 +1,43 @@
+import Link from "next/link";
 import React from "react";
+import styles from "./sign-in.module.css";
 
 export const SignIn: React.FC = () => {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <div id={styles.container}>
+        <div id={styles.title}>
+          <p className="font-semibold text-5xl py-3">Sign In</p>
+          <p className="pb-3">Sign in to continue with Smarter!</p>
+        </div>
+        <form id={styles.signInForm}>
+          <input
+            className={styles.textBox}
+            type="email"
+            placeholder="Your email"
+          />
+          <br />
+          <input
+            className={styles.textBox}
+            type="password"
+            placeholder="Your password"
+          />
+          <br />
+          <input id={styles.btnSubmit} type="submit" />
+        </form>
+        <hr />
+        <div id={styles.otherActions}>
+          <p className="text-xl font-bold text-gray-400">
+            <Link href="#">Forgot password?</Link>
+          </p>
+          <p className="text-xl">
+            Do not have an account?{" "}
+            <span className="text-blue font-bold">
+              <Link href="#">Sign Up!</Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
